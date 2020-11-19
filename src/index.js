fea277512ce660950553c333b2757af928f03cdb
Renee Labutay <reneelabutay@ucsb.edu>
2020-11-18 17:13:09 -0800

    added code to deploy to github pages

--- src/index.js
+++ src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
+
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+/*
+ReactDOM.render(
+  <Auth0Provider
+    domain="YOUR_DOMAIN"
+    clientId="C0HdfKBkYoNnIhsg6TWPkGOKgUs3yXSt"
+    redirectUri={window.location.origin}
+  >
+    <App />
+  </Auth0Provider>,
+  document.getElementById("root")
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
