801dfdfc79bd8975f996e0de65351c090ed0482a
Regina Weinreb <rweinreb@ucsb.edu>
2020-11-22 23:25:38 -0800

    added Auth0 functionality

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-
+import LoginButton from './LoginButton'
 export class Login extends Component {
 
-    constructor() {
+  /*constructor() {
 		super();
 		this.state = {
             email: '',
@@ -15,15 +15,15 @@ export class Login extends Component {
 		this.setState({
 			[e.target.name]: e.target.value
 		});
-    }
+    }*/
 
-    handleSubmit(e) {
+   /* handleSubmit(e) {
         e.preventDefault();
         /* 
         this is where you take the data 
         and perform user authentication to the backend
         */
-    }
+   // } 
 
     render() {
         return(
@@ -33,16 +33,8 @@ export class Login extends Component {
                     <div className="login-title">
                         Member Login
                     </div>
-                    <div>
-                        <form onClick={this.handleSubmit} className="login-form">
-                            <input type="text" name="email" className="login-input" 
-                                placeholder="Email" onChange={this.handleChange} value={this.state.email}>  
-                            </input>
-                            <input type="password" name="password" className="login-input"
-                                placeholder="Password" onChange={this.handleChange} value={this.state.password}>  
-                            </input>
-                            <button className="submit-button">Login</button>
-                        </form>
+                    <div className="login-button">
+                        <LoginButton/>
                     </div>
                 </div>
             </div>
