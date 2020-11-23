e3eec4f57b39b94c3ac06080d21a88af7075b1d8
Renee Labutay <reneelabutay@ucsb.edu>
2020-11-22 22:56:45 -0800

    updated packages

--- server/tokens.js
+++ server/tokens.js
@@ -10,16 +10,6 @@ const generateToken = config => {
   );
 };
 
-const chatToken = (identity, config) => {
-    const chatGrant = new ChatGrant({
-      serviceSid: config.twilio.chatService
-    });
-    const token = generateToken(config);
-    token.addGrant(chatGrant);
-    token.identity = identity;
-    return token;
-};
-
 const videoToken = (identity, room, config) => {
   let videoGrant;
   if (typeof room !== 'undefined') {
@@ -33,22 +23,4 @@ const videoToken = (identity, room, config) => {
   return token;
 };
 
-const voiceToken = (identity, config) => {
-    let voiceGrant;
-    if (typeof config.twilio.outgoingApplicationSid !== "undefined") {
-      voiceGrant = new VoiceGrant({
-        outgoingApplicationSid: config.twilio.outgoingApplicationSid,
-        incomingAllow: config.twilio.incomingAllow
-      });
-    } else {
-      voiceGrant = new VoiceGrant({
-        incomingAllow: config.twilio.incomingAllow
-      });
-    }
-    const token = generateToken(config);
-    token.addGrant(voiceGrant);
-    token.identity = identity;
-    return token;
-};
-
-module.exports = { chatToken, videoToken, voiceToken };
+module.exports = { videoToken };
